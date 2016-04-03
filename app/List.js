8db4673558721361cb14782c0772cf8a69a8738a
Evan Palmer <evjpalmer@gmail.com>
2016-04-03 13:53:19 -0700

    make List a class component, add lifecycle methods to adjust scrollHeight

--- app/List.js
+++ app/List.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { PropTypes } from 'react';
 import ListItem from './ListItem';
 
-const List = (props) => {
-  const listItems = props.items.map(
-    (item, i) => (
-      <ListItem
-        key={i}
-        num={i}
-        text={item.text}
-        done={item.done}
-        toggleDone={props.toggleDone}
-        removeItem={props.removeItem}
-      />
+class List extends React.Component {
+  componentWillUpdate() {
+    let node = ReactDOM.findDOMNode(this);
+    this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
+  }
+  componentDidUpdate() {
+    if (this.shouldScrollBottom) {
+      let node = ReactDOM.findDOMNode(this);
+      node.scrollTop = node.scrollHeight - node.clientHeight;
+    }
+  }
+  render() {
+    const listItems = this.props.items.map(
+      (item, i) => (
+        <ListItem
+          key={i}
+          num={i}
+          text={item.text}
+          done={item.done}
+          toggleDone={this.props.toggleDone}
+          removeItem={this.props.removeItem}
+        />
+      )
     )
-  )
-  return (
-      <ul className={listItems.length > 0 ? 'display-items' : 'display-none'}>
-        {listItems}
-      </ul>
-  );
+    return (
+        <ul className={listItems.length > 0 ? 'display-items' : 'display-none'}>
+          {listItems}
+        </ul>
+    );
+  }
 }
 
 List.propTypes = {
